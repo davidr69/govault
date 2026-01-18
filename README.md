c8079654e0e033118ded2f998c4859ec7e047659
David Rosario <david@lavacro.com>
2026-01-17 23:58:18 -0500

    initial checking

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # govault
+
+This is a Go port of the Java/Spring Boot
+[vault-retreive](https://github.com/davidr69/vault-retrieve)
+application.
+
---
+++ main.go
@@ -0,0 +1,31 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log/slog"
+	"os"
+)
+
+const banner = `
+banner here
+`
+
+type Config struct {
+	ServiceAcctUser   string
+	ServiceAcctSecret string
+}
+
+func main() {
+	fmt.Print(banner)
+
+	slog.SetDefault(slog.New(slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		AddSource: true,
+	})))
+
+	var cfg Config
+
+	flag.StringVar(&cfg.ServiceAcctUser, "service-acct-user", "", "Service account username")
+	flag.StringVar(&cfg.ServiceAcctSecret, "service-acct-secret", "", "Service account secret")
+	flag.Parse()
+}
